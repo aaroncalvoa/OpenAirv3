639edd815638e212550f032874211c5e346eef60
Your Name <gaboruls56@gmail.com>
2022-10-21 00:50:22 -0300

    aaron cagon de mrd

---
+++ app/src/main/java/com/example/openair/RecordActivity.kt
@@ -0,0 +1,15 @@
+package com.example.openair
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record)
+    }
+    public fun mamapinga(){
+        var aaronmamaguevo= "las de 12 son god"
+        /// musica cañera
+    }
+}
